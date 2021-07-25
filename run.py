3dbb3a80afa6df6b0614ce4578404bedb7f95d9d
AlfaRiza <malfariza45@gmail.com>
2021-07-26 01:05:36 +0700

    ADD Flask in run.py

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/detik-populer')
+def populer():
+    html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
+
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    populers = soup.find('div', attrs={'class': 'grid-row list-content'})
+
+    titles = populers.findAll(attrs={'class': 'media__title'})
+    images = populers.findAll(attrs={'class': 'media__image'})
+    return render_template('index.html', images=images)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
